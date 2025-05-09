a46a0e2dd3cace2e1851c16057fc055ef4c98f64
Juan Felipe <juanfelipecardenasgonzalez@gmail.com>
2025-05-09 17:24:06 -0500

    update with enviroment variables

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
+// Cargar .env solo en desarrollo local
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const { db } = require('./config/firebase');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const productsRoutes = require('./routes/products.routes');
 const loginRoutes = require('./routes/login.routes');
-const serverless = require('serverless-http')
 
-// Cargar .env solo en desarrollo local
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+
+
+console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
+console.log('FIREBASE_PRIVATE_KEY (primeros 20 chars):', process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.substring(0, 20) : 'undefined');
+// Imprime otras variables clave también
+
 
 // Middleware
 app.use(cors());
@@ -31,5 +37,8 @@ app.use((err, req, res, next) => {
 });
 
 
+
+
+
 // Exportar la app para que Vercel la use como función serverless
-module.exports = serverless(app); // al final del archivo
+module.exports = app; 
