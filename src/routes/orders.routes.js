2e5b208db7f4fc3aa6044eefb96a75e6110b5146
Martin Pereira <135734700+mpereira11@users.noreply.github.com>
2025-05-17 17:54:36 -0500

    Regreso a anterior ruta

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, getOrderStatus, getOrders, getOrderHistory, updateOrderStatus, getAllOrdersForPOS} = require('../controllers/orders.controller');
+const { createOrder, getOrderStatus, getOrders, getOrderHistory, updateOrderStatus } = require('../controllers/orders.controller');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/', authMiddleware, createOrder);
 router.get('/:id/status', authMiddleware, getOrderStatus);
 router.get('/', authMiddleware, getOrders);
 router.get('/history', authMiddleware, getOrderHistory)
-router.get('/all', authMiddleware, getAllOrdersForPOS);
 router.patch('/:id/status', authMiddleware, updateOrderStatus);
 
 module.exports = router;
